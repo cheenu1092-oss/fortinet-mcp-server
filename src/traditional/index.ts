a061c3586fd0dec9766841b7a23ab686e84d5361
Cheenu <cheenu1092@gmail.com>
2026-03-31 07:49:38 -0700

    feat: Register service and VPN tools in traditional mode

--- src/traditional/index.ts
+++ src/traditional/index.ts
@@ -6,6 +6,8 @@ import { FortiGateClient } from "../client.js";
 import { registerFirewallTools } from "./tools/firewall.js";
 import { registerAddressTools } from "./tools/address.js";
 import { registerSystemTools } from "./tools/system.js";
+import { registerServiceTools } from "./tools/service.js";
+import { registerVpnTools } from "./tools/vpn.js";
 
 async function main() {
   const config = loadConfig();
@@ -25,6 +27,8 @@ async function main() {
   registerFirewallTools(server, client, config);
   registerAddressTools(server, client, config);
   registerSystemTools(server, client, config);
+  registerServiceTools(server, client, config);
+  registerVpnTools(server, client, config);
 
   const transport = new StdioServerTransport();
   await server.connect(transport);
